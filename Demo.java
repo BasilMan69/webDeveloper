ea051237f40f55a95af7210ef909aab5b85ceb9d
BasilMan69 <billkhoi2006@gmail.com>
2023-06-01 15:32:19 +0700

    Add feature to the demo

--- Demo.java
+++ Demo.java
@@ -1,6 +1,9 @@
 public class Demo {
     
     public static void main(String[] args) {
-
+        System.out.println("Starting Demo");
+        System.out.println("This is a demo");
+        System.out.println("Ending demo");
+        
     }
 }
